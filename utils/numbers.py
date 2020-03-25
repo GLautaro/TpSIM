9bc6b83fcdaa087b432332ead26caf02ebdea9de
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2020-03-25 18:39:25 -0300

    Basic structure

---
+++ utils/numbers.py
@@ -0,0 +1,7 @@
+# Modulo utilizado para desarrollar funciones referidas al manejo de numeros
+
+from random import random
+
+def generate_rand(n):
+    numbers_array = list(map(lambda x: round(x, 4), [random() for i in range(n)]))
+    return numbers_array
