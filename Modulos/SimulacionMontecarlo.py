5005247a81a7d4be60dec1cc012e241a3b443436
Agustin <chg.agustin@gmail.com>
2020-05-08 14:14:43 -0300

    Se calcula el puntaje y se envian los datos para la creacion del DataFrame de las ultimas dos iteraciones

--- Modulos/SimulacionMontecarlo.py
+++ Modulos/SimulacionMontecarlo.py
@@ -1,5 +1,5 @@
 from Modulos import TablasProbabilidad as tablas
-from Modulos.Utils import Truncate
+from Modulos.Utils import Truncate, CrearDataFrame
 import pandas as pd
 import random 
 
@@ -9,9 +9,9 @@ def SimulacionBowling(vector_primera_bola,
                       vector_segunda_bola_8,
                       vector_segunda_bola_9,
                       it,ron,
-                      punt_objetivo, punt_10_prim,punt_10_seg
+                      puntaje_objetivo, punt_10_prim,punt_10_seg,
                       mostrar_desde,mostrar_cantidad
-                      )
+                      ):
     int_pr = tablas.CrearProbabilidadesAcumuladas(vector_primera_bola)
     int_seg_7 = tablas.CrearProbabilidadesAcumuladas(vector_segunda_bola_7)
     int_seg_8 = tablas.CrearProbabilidadesAcumuladas(vector_segunda_bola_8)
@@ -19,19 +19,54 @@ def SimulacionBowling(vector_primera_bola,
     
     cant_exitos = 0
     df = pd.DataFrame(columns=['Exito',])
+    iteraciones = []
     for i in range(it):
         iteracion = "It º" + str(i)
+        puntajeTotal = 0
+        ronda_datos = [i]
         for j in range(ron):
             ronda = "Ronda " + str(j)
             tirada1 = ronda + " tirada 1"
             tirada2 = ronda + " tirada 2"
-
             puntaje = 0
-            ran = Truncate(random.uniform(0,1.00001), 4)
-            cant_pinos = tablas.CalcularPinosTirada(int_pr, ran)
+            rand_tirada_1 = Truncate(random.uniform(0,1.00001), 4)
+            cant_pinos = tablas.CalcularPinosTirada(int_pr, rand_tirada_1)
+            rand_tirada_2 = Truncate(random.uniform(0,1.00001), 4)         
 
+            if(cant_pinos == 7):                
+                 cant_pinos_seg = tablas.CalcularPinosTirada(int_seg_7, rand_tirada_2, primera_tirada=False)
+            elif(cant_pinos==8):
+                cant_pinos_seg = tablas.CalcularPinosTirada(int_seg_8, rand_tirada_2, primera_tirada=False)
+            else:
+                 cant_pinos_seg = tablas.CalcularPinosTirada(int_seg_9, rand_tirada_2, primera_tirada=False)
+            if(cant_pinos == 10):
+                puntaje = punt_10_prim
+            elif((cant_pinos + cant_pinos_seg) == 10):
+                puntaje = punt_10_seg
+            else:
+                puntaje = cant_pinos + cant_pinos_seg
 
-            if puntaje >= puntaje_objetivo:
-                cant_exitos += 1
+            puntajeTotal += puntaje
 
+            if(i+1 >= it-1):
+                ronda_datos.extend([rand_tirada_1, cant_pinos, rand_tirada_2, cant_pinos_seg, puntajeTotal])
+
+        if(len(ronda_datos)>1):
+            iteraciones.append(ronda_datos)
+            
+        if puntajeTotal >= puntaje_objetivo:
+            cant_exitos += 1
+    
     probabilidad_exito = cant_exitos / it
+    
+    return CrearDataFrame(ron, iteraciones)
+
+def testSimulacionMonte():
+    return SimulacionBowling([1,2,3,4,4],
+                     [1,2,3,4,4],
+                      [1,2,3,4,4],
+                      [1,2,3,4,4],
+                      5,5,
+                      120, 125,222,
+                      2,2
+                      )
--- Modulos/Utils.py
+++ Modulos/Utils.py
@@ -1,6 +1,7 @@
 from openpyxl import Workbook,load_workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 import pandas as pd
+import numpy as np
 def Truncate(n, decimals=0):
     multiplier = 10 ** decimals
     return int(n * multiplier) / multiplier
@@ -16,25 +17,25 @@ def GenerarExcel(dict_dataframes, nombre_archivo):
 def ValidarVectorProbabilidades(vector):
     return (sum(vector) == 1)
 
-def CrearDataFrame(ron):
+def CrearDataFrame(ron, datos):
     col = ["Iteracion"]
     for i in range(ron):
         t1 = "ronda"+str(i+1)+"_tirada1"
         t2 = "ronda"+str(i+1)+"_tirada2"
-        pun_t1 = t1+"_punt"
-        pun_t2 = t2+"_punt"
+        ##pun_t1 = t1+"_punt"
+        ##pun_t2 = t2+"_punt"
         ran_t1 = "rand_" + t1
         rand_t2 = "rand_" + t2
-        ac_t1 = pun_t1 + "_ac" 
-        ac_t2 = pun_t2 + "_ac"
+        ##ac_t1 = pun_t1 + "_ac" 
+        ##ac_t2 = pun_t2 + "_ac"
         ac_rond = "ronda_"+str(i+1)+"_punt_ac"
-        col = col + [ran_t1,t1,pun_t1,rand_t2,t2,pun_t2,ac_t2,ac_rond]
-    col = col + ["puntaje_final"]
+        col = col + [ran_t1,t1,rand_t2,t2,ac_rond]
+    ##col = col + ["puntaje_final"]
     d = {}
     for c in col:
         d[c] = ["null","null1","null2"]
 
-    df = pd.DataFrame(d,columns=col)
+    df = pd.DataFrame(np.array(datos),columns=col)
     return df
 
     
