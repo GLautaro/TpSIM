8fab988e262700f1d663fb0975eef10b7be75f8e
Agustin <chg.agustin@gmail.com>
2020-06-27 15:52:11 -0300

    Fix EcDif

--- Soporte/EcDif.py
+++ Soporte/EcDif.py
@@ -3,15 +3,18 @@ import numpy as np
 
 def euler(h, a0, t0):
     r = []
-    a = a0
-    a1=0
+    a=a0
+    a1=a0
+    t1=0
     t=t0
-    while (a > 0):
+    while (a>0):
+        a=a1
+        t=t1
         da = -68-((a*a)/a0)
         a1 = a + h*da
-        t = t + h
-        r.append([t-h,a,da,t,a1])
-        a=a1
+        t1 = t + h
+        r.append([t,a,da,t1,a1])
+
     df2 = pd.DataFrame(np.array(r),
                    columns=['t', 'a', 'da/dt', 't+1', 'D+1'])
     return df2
